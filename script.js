f2b8954001054ee69df1268c919a59d6cebf946a
Glitch (glitch-hello-website) <none>
2022-09-25 02:53:12 +0000

    🌔👝 Checkpoint ./script.js:49107094/96

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ if (btn) {
   btn.onclick = function() {
     // The JS works in conjunction with the 'dipped' code in style.css
     btn.classList.toggle("dipped");
+    btn.location.href='https://google.com';
   };
 }
 
